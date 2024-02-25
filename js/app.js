9e39f3be52fc3cd78916c522a59eaade9184e447
ItsCodeOutside <24233088+ItsCodeOutside@users.noreply.github.com>
2024-02-25 10:23:55 +0000

    Added comments, updated example content to remove braces message, renamed function to load content

--- js/app.js
+++ js/app.js
@@ -1,36 +1,50 @@
-let deferredPrompt;
+/*
+app.js is the main JavaScript file for the app. It handles navigation and deciding how to handle the install prompt.
+*/
+
+// Global variables
+/*
+    allowInstall   - Whether to allow the install prompt to be shown or not
+    linkIds        - ID of each nav link, must be manually updated here for now
+    installLinkShown - Tracks if install link has been shown or not
+    appRunMode     - Overridden by version.txt in checkVersion(), can be used as a flag for extra debug info
+    navToggle      - The SVG hamburger menu button
+    deferredPrompt - Tracks the install prompt event
+*/
+const allowInstall = true;
+const linkIds = ['homeLink', 'aboutLink', 'tosLink', 'brandLink'];
 let installLinkShown = false;
 let appRunMode = 'debug';
 let navToggle = document.getElementById('navbar-toggler');
-const allowInstall = true;
-const linkIds = ['homeLink', 'aboutLink', 'tosLink', 'brandLink'];
+let deferredPrompt;
 
+// Show the browser's install prompt when user clicks the install link
 function showInstallPrompt(e) {
     e.preventDefault();
 	if (deferredPrompt) {
 		deferredPrompt.prompt();
 		deferredPrompt.userChoice.then((choiceResult) => {
 			if (choiceResult.outcome === 'accepted') {
-				console.log('User accepted the A2HS prompt');
+                // Remove install links when PWA is installed
                 document.getElementById('nav-install-pwa').remove();
                 document.getElementById('footer-install-pwa').remove();
-
-			} else {
-				console.log('User dismissed the A2HS prompt');
+                deferredPrompt = null;
 			}
-			deferredPrompt = null;
+            // You can do something when the user rejects installation with an 'else' here
 		});
 	}
 }
 
-function loadTemplate(template) {
-	document.getElementById('content').innerHTML = template;
+function loadContent(data) {
+	document.getElementById('content').innerHTML = data;
 }
 
+// changes the URL bar & adds to browse history so it looks like a real page change
 function updateAddressBar(url) {
 	history.pushState(null, null, url);
 }
 
+// When the site is opened from a link or history, the 'current' page will be in the query string's "p" parameter
 function getPageFromQueryString() {
 	const urlParams = new URLSearchParams(window.location.search);
 	let page = urlParams.get('p');
@@ -38,21 +52,23 @@ function getPageFromQueryString() {
         getPage('home'); 
     }
     else {
-        // Remove any trailing hash
+        // Remove any trailing hash - sometimes added by refreshes or links within pages
 	    getPage(page.replace(/#$/, ''));
     }
 }
 
+// When a nav link is clicked, get the page and update the address bar
 function getPageByLinkDataTag(e) {
     getPage(e.srcElement.attributes['data-tag'].value);
 }
 
+// Attempt to fetch the page and load its content. When PWA is installed, `fetch()` is intercepted and checks the cache first
 function getPage(pageName) {
     const pageUrl = `/pages/${pageName}.html`;
     fetch(pageUrl).then(response => {
         if(response.ok) {
             response.text().then(content => {
-                loadTemplate(content);
+                loadContent(content);
                 updateAddressBar(`?p=${pageName}`);
             });
         }
@@ -60,35 +76,47 @@ function getPage(pageName) {
             throw new Error('Server returned: ' + response.status + ' ' + response.statusText);
         }
     }).catch(error => {
-        console.log('fetch error: ' + error);
+        if (appRunMode == 'debug')
+            console.log('fetch error: ' + error);
     });
 }
 
+// Check version and see if we're in debug mode
 function checkVersion() {
     if (!navigator.onLine) {
         //use old version number
-        console.log('Offline mode');
+        if (appRunMode == 'debug')
+            console.log('Offline mode');
     }
     else {
         fetch('/version.txt')
         .then(result => {
+            // Set debugData to whatever you want to display in the footer while in debug mode
+            let debugData = "";
             result.text().then((data) => {
-                if (data.substring(0, 6) == 'debug-') { appRunMode = 'debug'; }
-                console.log(data);
-                document.getElementById('appVersion').innerHTML = data;
+                if (data.substring(0, 6) == 'debug-') { 
+                    appRunMode = 'debug'; 
+                }
+                document.getElementById('appVersion').innerHTML = `${data} ${debugData}`;
             });
         });
     }
 }
 
+// Event listeners
+//----------------
+
+// When all content is loaded...
 document.addEventListener('DOMContentLoaded', function() {
+
+    // Add listener for the PWA install prompt so we can show nice install links to the user
     window.addEventListener('beforeinstallprompt', (e) => {
         e.preventDefault();
         deferredPrompt = e;
 
         if (allowInstall && !installLinkShown) {
             installLinkShown = true;
-            // Show an install link or call showInstallPrompt() directly
+            // create an install link in the footer
             const installLink = document.createElement('a');
             installLink.id = 'footer-install-pwa';
             installLink.classList.add('install-pwa');
@@ -97,6 +125,7 @@ document.addEventListener('DOMContentLoaded', function() {
             installLink.addEventListener('click', showInstallPrompt);
             document.getElementsByTagName('footer')[0].appendChild(installLink);
 
+            // create an install link in the nav
             const installLink2 = installLink.cloneNode(true);
             const navItem = document.createElement('li');
             navItem.classList.add('nav-item');
@@ -108,25 +137,28 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-// Add event listeners to the nav links
-linkIds.forEach(id => {
-    document.getElementById(id).addEventListener('click', (e) => {
-        e.preventDefault();
-        getPageByLinkDataTag(e);
+    // Add event listeners to the nav links
+    linkIds.forEach(id => {
+        document.getElementById(id).addEventListener('click', (e) => {
+            e.preventDefault();
+            getPageByLinkDataTag(e);
+        });
     });
-});
 
 
     // Call getPageFromQueryString on page load
     getPageFromQueryString();
     checkVersion();
-    // Load the home template by default
+
+    // Check fo the serviceworker and register it
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', function() {
             navigator.serviceWorker.register('/sw.js').then(function(registration) {
-                console.log('Service Worker registered with scope:', registration.scope);
+                if (appRunMode == 'debug')
+                    console.log('Service Worker registered with scope:', registration.scope);
             }, function(err) {
-                console.log('Service Worker registration failed:', err);
+                if (appRunMode == 'debug')
+                    console.log('Service Worker registration failed:', err);
             });
         });
     }
--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
+/* 
+sw.js is the Service Worker of the Progressive Web App.
+When installed, this JavaScript file will be run in the background to cache all the assets needed for the app to work offline and intercept `fetch()` requests.
+*/
+
+// Choose whether the cache will be used or not (should be true)
+const useCache = true;
+// List all items that should be cached. All assets for all pages must be listed here for the app to work offline.
 const cachableItems = [
     '/',
     '/manifest.json',
@@ -13,6 +21,8 @@ const cachableItems = [
     '/pages/tos.html',
     '/version.txt'
 ]
+
+// When the user installs the PWA, add the cachable items to the cache
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('app-cache').then(function(cache) {
@@ -21,10 +31,12 @@ self.addEventListener('install', function(event) {
     );
 });
 
+// Immediately claim the pages so that the service worker takes control of the pages
 self.addEventListener('activate', () => {
     self.clients.claim();
 });
 
+// Log messages/push notifications to the console. These can be generated from dev tools in the browser for debugging
 self.addEventListener('message', (event) => {
   console.log(`message: ${event.data?.text()}`);
 });
@@ -33,7 +45,7 @@ self.addEventListener('push', (event) => {
   console.log(`push :${event.data?.text()}`);
 });
 
-let useCache = true;
+// When the user requests a page, check if it is fresh in the cache (<1d old). If it is, check if it's stale and either return it. If it is not, fetch it and add it to the cache.
 self.addEventListener('fetch', function(event) {
     return new Promise((resolve) => {
         const fullUrl = new URL(event.request.url);
@@ -42,7 +54,7 @@ self.addEventListener('fetch', function(event) {
             const lastUpdate = result;
             const oneDay = 86400000; // 24 hours as milliseconds
             const diff = Date.now() - lastUpdate;
-            if (diff > oneDay) {
+            if (!useCache || diff > oneDay) {
                 fetch(event.request).then((response) => {
                     caches.open('app-cache').then((cache) => {
                         cache.put(event.request, response.clone());
@@ -56,29 +68,47 @@ self.addEventListener('fetch', function(event) {
     });
 });
 
+
+// -- This is the IndexDB interface --
+// IndexDB uses callbacks instead of promises
+// -----------------------------------
+
+
+// Open the database
 let browserIndexDb = indexedDB.open('db', 1);
+
+// Add the onupgradeneeded callback to create the schema
 browserIndexDb.onupgradeneeded = function() {
   let db = browserIndexDb.result;
   if (!db.objectStoreNames.contains('lastUpdate')) {
     db.createObjectStore('lastUpdate');
   }
 };
+
+// On success of opening the database, add the last update time for each cachable item
 browserIndexDb.onsuccess = function(e) {
     cachableItems.forEach((item) => {
         setLastUpdate(item);
     });
 }
+
+// Log any errors to console. This should be replaced with something informing the user.
 browserIndexDb.onerror = function() {
     console.log('Error: ' + browserIndexDb.error);
 }
+
+// Make it easy to get the last update time for a "page" (cachableItem)
 function getLastUpdate(pageUrl) {
+    // Return a promise to wrap the callback-based indexedDB
     return new Promise((resolve, reject) => {   
         let db = browserIndexDb.result;
         let transaction = db.transaction('lastUpdate', 'readwrite');
         let store = transaction.objectStore('lastUpdate');
+        // Request the last update time for the given page
         let request = store.get(pageUrl);
 
         request.onsuccess = function() {
+            // When callback hit, resolve the promise with the result
             resolve(request.result);
 
         request.onerror = function() {
@@ -86,6 +116,8 @@ function getLastUpdate(pageUrl) {
         }
     }});
 }
+
+// Make it easy to set the last update time for a "page" (cachableItem), no promises needed because we don't need to wait for a result
 function setLastUpdate(pageUrl) {
     let db = browserIndexDb.result;
     let transaction = db.transaction('lastUpdate', 'readwrite');
